a715238230f10acad0aa58c955b1c045921d8815
ShanmuCode27 <90304223+ShanmuCode27@users.noreply.github.com>
2023-03-11 12:06:10 +0530

    Added button navigation to second screen

--- app/src/main/java/com/shanmu/schedulemaker/GetGoals.java
+++ app/src/main/java/com/shanmu/schedulemaker/GetGoals.java
@@ -4,9 +4,13 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,33 +19,17 @@ import android.view.ViewGroup;
  */
 public class GetGoals extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    String goalName;
+    EditText goal;
 
     public GetGoals() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment GetGoals.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static GetGoals newInstance(String param1, String param2) {
+    public static GetGoals newInstance() {
         GetGoals fragment = new GetGoals();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,16 +37,35 @@ public class GetGoals extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_get_goals, container, false);
+        View view = inflater.inflate(R.layout.fragment_get_goals, container, false);
+
+        goal = (EditText) view.findViewById(R.id.input_text_goal);
+
+        goal.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                goalName = "";
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // No code
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                goalName = goal.getText().toString();
+                Log.i("goal is ", goalName);
+            }
+        });
+
+        return view;
     }
 }
